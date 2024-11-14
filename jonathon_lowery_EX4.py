fb718c65e595f0fba81ba79137d689a5e6011e79
JON2149268 <JON2149268@maricopa.edu>
2024-11-13 21:29:46 -0700

    Add files via upload

---
+++ jonathon_lowery_EX4.py
@@ -0,0 +1,42 @@
+# Jonathon Lowery
+# CIS256 Fall 2024
+# EX 4
+
+import random
+
+# create predefined list of words for user to guess
+words = ['attorney', 'good', 'evil', 'science', 'fiction', 'reading', 'rainbow', 'hospital', 'instrument', 'equipment', 'land', 'water', 'mountain', 'coding', 'shocked', 'nomination', 'resignation', 'president', 'america']
+
+# generate a random word from the word list and prompt user to guess the word
+# one letter at a time
+word = random.choice(words)
+print(f"Guess the word, one letter at a time.")
+
+guesses = ' '
+turns = 15
+
+while turns > 0:
+    failed = 0
+    for char in word:
+        if char in guesses:
+            print(char, end=" ")
+
+        else:
+            print("_")
+            failed += 1
+
+    if failed == 0:
+        print(f"Congratulations! You won the game!!")
+        print(f"The word is: ", word)
+        break
+
+    print()
+    guess = input(f"Guess a character:")
+    guesses += guess
+    if guess not in word:
+        turns -= 1
+        print(f"Incorrect guess")
+        print(f"You have", + turns, "more guesses.")
+        if turns == 0:
+            print(f"Sorry, you lost the game. Better luck next time.")
+            
